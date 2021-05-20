095db7ed8d488aab91aab6da6571edc657da5520
notimportant <torben.q@web.de>
2021-05-20 14:32:29 +0200

    Dateien + rename + folder clean +

---
+++ 01_variablen/variablen 01-04.js
@@ -0,0 +1,102 @@
+/***** Variablen 01 *******/
+
+// = 1 Zeilen Kommentar
+
+/**  = Block Kommentar
+ 
+    = Block Kommentar
+
+ */
+
+  // Ausgabe  
+  //console.log("Hello World!");
+  //console.log("firstName") // Camel Schreibweise;
+
+  // Deklaration + Wertzuweisung I 
+  /* // aus Kommentiert 
+  let firstName; // Deklarartion (Definition)
+  firstName = "Max"; // Wertzuweisung
+  console.log(firstName); // Ausgabe 
+  let familyName = "Mustermann"; // Deklaration + Wertzuweisung
+  console.log(familyName); // Ausgabe 
+  console.log(firstName+" "+familyName);
+  */
+
+  // Deklaration + Wertzuweisung II
+  /*
+  let firstName,familyName;
+  firstName = prompt ("Bitte Vornamen eingeben:");
+  familyName = prompt ("Bitte Nachnamen eingeben");
+  console.log("Hallo," + firstName + " " + familyName + "!");
+  */
+
+  // JS JavaScript ist eine untypisierte Sprache! | untyped
+  /*
+  let test; // Deklaration
+  test = "Hi"; // string - Zeichenketten
+  test = 2; // number - numerische Werte 
+  //test = true; // boolean - 1 Bit(s) true or false
+  console.log("Typ: " + typeof test);
+  console.log("Inhalt: " + test);
+  */
+
+  /***** Variablen 02 *****/
+  /*
+  // Deklaration
+  let ageFrank, ageMark;
+  let birthYearFrank, birthYearMark;
+
+  // Wertzuweisung | Assignment
+  //let year = 2021;
+  let date = new Date();
+  let year = date.getFullYear();
+  //console.log(year);
+  //console.log(date);
+  ageFrank = 25;
+  ageMark = 30;
+
+  //Operation (Differenz)
+  birthYearFrank = year - ageFrank;
+  birthYearMark = year - ageMark;
+
+  console.log("Aktuelles Jahr: " + year);
+  console.log("birthYearFrank: " + birthYearFrank);
+  console.log("birthYearMark:" + birthYearMark);
+  */
+
+  /***** Variablen 03 *****/
+  /*
+  let ageFrank, ageMark;
+  ageFrank = 25;
+  ageMark = 30;
+
+  // Operation (Vergleich/Test)
+  let isFrankOlder = (ageFrank > ageMark);
+  console.log(isFrankOlder);
+  */
+
+  /***** Variablen 04 *****/
+  // Zusammenfassung Datentypen
+  /*
+  let nix;
+  let ageFrank = 25;
+  let firstName = "Max";
+  let isJohnOlder = (10 > 5);
+
+  console.log ("nix: " + typeof nix);
+  // = nix: undefined
+  console.log ("ageFrank: " + typeof ageFrank);
+  // = ageFrank: number
+  console.log ("firstName: " + typeof firstName);
+  // = firstName: string
+  console.log ("isJohnOlder: " + typeof isJohnOlder);
+  // = isFrankOlder: boolean
+  */
+ 
+
+
+
+
+
+
+  
---
+++ 03_Funktionen/funktionen.js
@@ -0,0 +1,114 @@
+
+/***** Funktionen 01 *****/
+
+// 1. Kapselung von Codeblöcken
+
+// Funktionsrumpf (body)
+function test()
+{
+    console.log("Hello World!");
+    //console.log("Hello Visitor!");
+    //console.log("Hello Germany!");
+}
+
+// Funktionsaufruf (call)
+//test();
+//prompt();
+//parseFloat();
+
+/***** Funktionen 02a *****/
+
+// 2a. Parametrisierung + Datenübergabe von INNEN
+function ausgabeNamen() 
+{
+   // let firstName = "Frank"; // Variable --> lokal scope
+   // console.log("Hello " + firstName + "!"); // Strukturänderung selbe Ausgabe wie //console.log("Hello Frank!");
+    
+}
+
+//ausgabeNamen();
+//console.log(firstName); // Fehler --> lokal!!!
+
+/***** Funktionen 02b *****/
+// 2b. Parametrisierung + Datenübergabe von AUSSEN
+
+function ausgabeNamenParam(firstName) // Parameter
+{
+    // wenn/if firstName leer , dann "nobody"
+    if (firstName == undefined || firstName == "")     // || = oder 
+    {
+    firstName = "nobody";    
+    }
+    
+     console.log("Hallo " + firstName + "!");
+}
+
+// ausgabeNamenParam("Deniz"); // Call + Argument(e)        // Z.46-49 markieren + strg # drücken ein/aus
+// ausgabeNamenParam("Frank");
+// ausgabeNamenParam();
+// ausgabeNamenParam(prompt("Bitte Namen eingeben:"));
+
+/***** Funktionen 02c *****/
+// 2c. Mehrere Parameter
+
+function ausgabeNamenParams(firstName, familyName) // Parameter
+{
+    console.log("Hallo " + firstName + " " + familyName + "!");
+}
+// ausgabeNamenParams("Frank","Ziehm");         // strg + #
+// ausgabeNamenParams("Ziehm","Frank");
+// ausgabeNamenParams(prompt("Bitte Vornamen eingeben") , prompt("Bitte Namen eingeben"));
+
+/***** Funktionen 03a *****/
+// 03a. Vorbereitung
+// Postulat: one function = one job (uncle Bob) in german: Was nicht passt, wird passend gemacht!
+// SRP single responsibility principle 
+
+function ausgabeNamenParams2(firstName, familyName) // Parameter
+{
+    // 1. Job: string composing
+    
+    let gap = " ";
+    let outputString = "Hallo " + firstName + gap + familyName + "!";
+
+    // 2. Job: output
+
+    //console.log("Hallo " + firstName + gap + familyName + "!");
+    console.log(outputString);
+
+}
+
+//ausgabeNamenParams2("Frank","Ziehm");
+
+/***** Funktionen 03b *****/
+
+
+/** 1.Job: String zusammensetzen */
+output(getString("Frank","Ziehm")); // Test
+//output(prompt("??"));
+function getString(firstName,familyName) 
+{
+    let gap = " ";
+    let outputString = "Hallo " + firstName + gap + familyName + "!";
+    return outputString; // Daten ----> an die Stelle des calls     
+}
+
+
+/** 2.Job: Ausgabe */
+// output("hi"); // Unit-Test  TDD Test driven Development
+// output("hi"); // Unit-Test  TDD Test driven Development
+// output("hi"); // Unit-Test  TDD Test driven Development
+// output("hi"); // Unit-Test  TDD Test driven Development
+function output(outputString) // function output(params)
+{
+    console.log(outputString);
+}
+
+
+
+
+
+
+  
+
+
