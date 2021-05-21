0e1a4f3f9345dc5c9357ba4902882898221e9bcb
notimportant <torben.q@web.de>
2021-05-21 11:49:59 +0200

    division refactoring ++

--- 04_Funktionen/funktionen_02.js
+++ 04_Funktionen/funktionen_02.js
@@ -4,9 +4,36 @@
 0. a+b / a-b/ a*b / a/b  // ergebnis c
 1. Dateneingabe + -überprüfung :: 
 2. Auswahl Rechenart :: 
-3. Fkt. Grundrechenarten :: 
-4. Ausgabe in Konsole :: 
+3. Fkt. Grundrechenarten :: check!
+4. Ausgabe in Konsole :: check!
 */
+
+
+// Modul: Division a / b | Test: | refactoring
+//ausgabe(dividieren(2,2));
+//ausgabe(dividieren(0,5));
+//ausgabe(dividieren(5,0));
+function dividieren(a,b) {
+    if(b!=0)
+    {
+        return a/b;
+    }
+    return "Dividieren durch 0 nicht möglich";
+    
+}
+
+// Modul: Division a / b | Test:
+//ausgabe(dividieren(2,2));
+//ausgabe(dividieren(0,5));
+//ausgabe(dividieren(5,0));
+function dividieren(a,b) {
+    if(b==0){ // wenn b = 0 // dann Fehlermeldung
+        return "Dividieren durch 0 nicht möglich";
+    }
+    else{
+        return a/b;
+    }
+}
 
 // Modul: Multiplikation a * b | Test:
 //ausgabe(multiplizieren(2,2));
@@ -14,7 +41,6 @@
 function multiplizieren(a,b) {
     return a * b;
 }
-
 
 // Modul: Subtraktion a - b | Test:
 //ausgabe(subtrahieren(2,1));
@@ -26,10 +52,10 @@ function subtrahieren(a,b) {
 //ausgabe(addieren(2,1));
 function addieren(a,b) {
     return a + b; 
- }
+}
  
  // Modul: Konsolenausgabe |  Test:
  //ausgabe("Hallo Welt");
  function ausgabe(outputStr) {
      console.log(outputStr);
- }
+}
