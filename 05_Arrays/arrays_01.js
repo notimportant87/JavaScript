3978a77e429ddff1bca4a5cd1db4706647768bcf
notimportant <torben.q@web.de>
2021-05-25 09:00:29 +0200

    get sentence +

--- 05_Arrays/arrays_01.js
+++ 05_Arrays/arrays_01.js
@@ -4,7 +4,16 @@
 /*** 01. Funktionalität mit Einzelparametern */
 // --> "Ich bin Max Mütze."
 //ausgabe(getSentence("Ich","bin","Max","Mütze")); // Test
-
+function getSentence(word1,word2,word3,word4) {
+    let gap = " ";
+    let punct = ".";
+    let str =   word1 + gap +
+                word2 + gap +
+                word3 + gap +
+                word4 +
+                punct;
+    return str;
+}
 
 // Modul: Ausgabe in Konsole | Test:
 //ausgabe("hi");
