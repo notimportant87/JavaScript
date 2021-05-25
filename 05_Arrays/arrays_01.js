d7612b1d65bfeed30fab30ca9e122770a80965b8
notimportant <torben.q@web.de>
2021-05-25 08:46:35 +0200

    txt + , rename

---
+++ 05_Arrays/arrays_01.js
@@ -0,0 +1,13 @@
+/* Die Satzbau-Maschine | Arrays */
+
+
+/*** 01. Funktionalität mit Einzelparametern */
+// --> "Ich bin Max Mütze."
+//ausgabe(getSentence("Ich","bin","Max","Mütze")); // Test
+
+
+// Modul: Ausgabe in Konsole | Test:
+//ausgabe("hi");
+function ausgabe(outputStr) {
+    console.log(outputStr);
+}
