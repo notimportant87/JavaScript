e06b25bf8f978831549d1cc4151ffb0d80de809c
notimportant <84065041+notimportant87@users.noreply.github.com>
2021-05-28 22:19:14 +0200

    create .js

---
+++ 05_Arrays/arrays_04_Challenge_03.js
@@ -0,0 +1,29 @@
+/* Vorüberlegungen */
+// push() / pop()
+
+
+// push --> Daten rein ... (+)
+// let arr = [];  // new Array()
+// ausgabe(arr);
+// arr.push("Ich");
+// ausgabe(arr);
+// arr.push("bin");
+// ausgabe(arr);
+// arr.push("Peter");
+// ausgabe(arr);
+
+
+// pop() --> Daten raus ... (-)
+// ausgabe(arr.pop());
+// ausgabe(arr);
+// ausgabe(arr.pop());
+// ausgabe(arr);
+// ausgabe(arr.pop());
+// ausgabe(arr);
+
+
+
+//ausgabe("hi");
+// function ausgabe(outputStr) {
+//     console.log(outputStr);
+// }
