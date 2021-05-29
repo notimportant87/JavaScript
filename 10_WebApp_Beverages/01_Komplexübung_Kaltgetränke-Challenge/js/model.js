90a655b86b111ffc38d0d9ad94a371e843c0567b
notimportant <torben.q@web.de>
2021-05-29 02:42:23 +0200

    add model.js +

---
+++ 10_WebApp_Beverages/01_Komplexübung_Kaltgetränke-Challenge/js/model.js
@@ -0,0 +1,13 @@
+/* model (Daten/Logik) */
+
+let data = {
+    milk:{lower:0, upper:5, bev:"milch"},
+    juice:{lower:6, upper:12, bev:"saft"},
+    cola:{lower:13, upper:20, bev:"cola"},
+    wine:{lower:21, upper:130, bev:"wein"},
+    default:{bev:"tee"}
+}
+
+let gui =   {
+            img:{path:"./bilder/", ext:".jpg"} 
+        }
